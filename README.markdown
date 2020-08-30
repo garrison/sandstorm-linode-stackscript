df614e5e9d0e42f7cc5f2c97b5b24d5054c9c029
Jim Garrison <jim@garrison.cc>
2020-08-30 16:12:32 -0400

    Initial working proof of concept

---
+++ README.markdown
@@ -0,0 +1,11 @@
+# Sandstorm one-click install on Linode
+
+This script provides a "one-click" install of [Sandstorm](https://sandstorm.io/) on a [Linode](https://www.linode.com/) instance, following Linode's instructions for making a "Community [StackScript](https://www.linode.com/products/stackscripts/)."
+
+Right now it serves as a working proof of concept.  Note that it does not check GPG signatures on the install script.
+
+Visit https://cloud.linode.com/stackscripts/666020 to deploy this script inside one's Linode account.
+
+## Notes
+
+Sandstorm's official installation instructions are available at https://sandstorm.io/install.  This StackScript uses the official Sandstorm install script.  It requires the user to choose a [Sandcats subdomain](https://docs.sandstorm.io/en/latest/administering/sandcats/) and to provide an email address for Sandcats and Let's Encrypt.  If the install fails for any reason (e.g., the desired sandcats.io name is already taken), the instance can be deleted before making a new deployment attempt.  After boot, it is necessary to `ssh` into the new server and run `sandstorm admin-token` once the installation has completed.  The [MOTD](https://en.wikipedia.org/wiki/Motd_(Unix)) should provide information on the current status of the script.
---
+++ sandstorm-linode-stackscript.bash
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+# This script provides a "one-click" install of Sandstorm.io on a
+# Linode instance, following Linode's instructions for making a
+# "Community StackScript."
+
+# First, we create a log for this script's output, following
+# https://www.linode.com/community/questions/18422/stackscript-logs
+exec >/var/log/sandstorm-install.log 2>&1
+
+# We provide a symlink in the home directory to the installation log,
+# in case /var/log is not an obvious enough location.
+ln -fs /var/log/sandstorm-install.log /root/sandstorm-install.log
+
+# Exit on failure.
+set -e
+
+# Edit /etc/motd to provide a message to the administrator upon ssh login.
+cat <<EOF >/etc/motd
+If you are seeing this, the Sandstorm install script is either running
+still or has failed.  You can follow its progress by running
+'tail -n +1 -f /var/log/sandstorm-install.log'.
+EOF
+
+# First, upgrade all packages.
+apt-get update
+apt-get upgrade -y
+
+# Linode will ask the user to enter responses to the following prompts
+# before the script is run:
+#
+# <UDF name="SANDCATS_SUBDOMAIN" label="Choose your desired Sandcats subdomain (alphanumeric, max 20 characters). https://sandcats.io/terms https://sandcats.io/privacy" example="some-unique-string" />
+# <UDF name="SANDCATS_EMAIL" label="We need your email on file so we can help you recover your sandcats domain if you lose access. No spam." example="user@example.com" />
+#
+# The references to these variables, below, must be in all caps - see
+# https://www.linode.com/community/questions/17528/stackscript-udf-variables#answer-67691
+
+# Download the Sandstorm install script
+INSTALL_SCRIPT=/root/sandstorm-install.sh
+curl -o $INSTALL_SCRIPT https://install.sandstorm.io
+
+# Install empty-except so that we can trick the Sandstorm install
+# script into thinking it is being run from a terminal.  Admittedly,
+# this is brittle, but it seems to be the most straightforward way to
+# have a working proof of concept of a one-click install script.
+# Unfortunately, empty-expect is no longer available in Ubuntu 20.04,
+# so having this work requires us to use an Ubuntu 18.04 image.
+apt-get install empty-expect
+
+# Run the Sandstorm install script, providing input using
+# empty-expect.  'empty -w' returns the number of matched
+# keyphrase-response pairs, so a return value of 1 is as expected.
+echo "Running empty-expect with a separate log file: /root/empty-expect.log"
+empty -L /root/empty-expect.log -f bash $INSTALL_SCRIPT
+empty -w "this Sandstorm install" "\n" || test $? -eq 1
+empty -w "OK to continue" "yes\n" || test $? -eq 1
+empty -w "sandcats.io subdomain" "${SANDCATS_SUBDOMAIN}\n" || test $? -eq 1
+empty -w "email address" "${SANDCATS_EMAIL}\n" || test $? -eq 1 # for Sandcats
+empty -t 30 -w "email address" "${SANDCATS_EMAIL}\n" || test $? -eq 1 # for Let's Encrypt
+
+# Update /etc/motd
+# Edit /etc/motd to have a login message
+echo All done.
+cat <<EOF >/etc/motd
+Sandstorm install is complete.  You can continue setup by running
+'sandstorm admin-token' and visiting the provided URL.
+EOF
+sandstorm admin-token
